078b28f9310f5116a92655abf04308bcb8415033
OksanaYur <107573946+OksanaYur@users.noreply.github.com>
2022-09-30 09:03:37 +0300

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios"; 
+import Loader from "react-loader-spinner";
+
+
+export default function Weather(props) {
+
+    function handleRespnse(response) {
+        //alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)} C`);
+    }
+    let apiKey = "fe80b84fc85eea0c9b1fafa5aa3bbced";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    
+axios.get(apiUrl).then(handleRespnse);
+    return (
+        <Loader
+        type="Puff"
+        color="blue"
+        height={100}
+        width={100}
+        timeout={3000} />
+    );
+}
