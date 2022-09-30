81a457072f934303ddd4cef949ce5f9042287a85
OksanaYur <107573946+OksanaYur@users.noreply.github.com>
2022-09-30 10:53:37 +0300

    loader fixing

--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,25 @@
 import React from "react";
-import axios from "axios"; 
-import Loader from "react-loader-spinner";
-
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
 
 export default function Weather(props) {
+  function handleRespnse(response) {
+    //alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)} C`);
+  }
+  let apiKey = "fe80b84fc85eea0c9b1fafa5aa3bbced";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
-    function handleRespnse(response) {
-        //alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)} C`);
-    }
-    let apiKey = "fe80b84fc85eea0c9b1fafa5aa3bbced";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    
-axios.get(apiUrl).then(handleRespnse);
-    return (
-        <Loader
-        type="Puff"
-        color="yellow"
-        height={100}
-        width={100}
-        timeout={3000} />
-    );
+  axios.get(apiUrl).then(handleRespnse);
+  return (
+    <Puff
+      height="80"
+      width="80"
+      radisu={1}
+      color="#FFFF00"
+      ariaLabel="puff-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
 }
