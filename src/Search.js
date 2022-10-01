1ec9dbfe4f084e152fed58d1159658fd52e8b254
OksanaYur <107573946+OksanaYur@users.noreply.github.com>
2022-10-01 14:35:00 +0300

    code updated

--- src/Search.js
+++ src/Search.js
@@ -1,56 +1,56 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function WeatherSearch(props) {
-const [city, setCity] = useState("");
-const [result, setResult] = useState("");
-const [weather, setWeather] = useState("");
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [result, setResult] = useState(false);
+  const [weather, setWeather] = useState({});
 
-function displayWeather(response) {
+  function displayWeather(response) {
     setResult(true);
-    setWeather(
-        {
-            temperature: response.data.main.temp,
-            wind: response.data.wind.speed,
-            humidity: response.data.main.humidity,
-            icon: `http://openweathermap.org/img/wn/${response.data.data.weather[0].icon}@2x.png`,
-            description: response.data.weather[0].description});
-        }
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
 
-        function handleSubmit(event) {
-            event.preventDefault();
-            let apiKey = "fe80b84fc85eea0c9b1fafa5aa3bbced";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "fe80b84fc85eea0c9b1fafa5aa3bbced";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
 
-  axios.get(apiUrl).then(handleSubmit);
-        }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
-        function updateCity(event) {
-            setCity(event.target.value);
-        }
-        let form = (
-            <form onSubmit={handleSubmit}>
-                <input type="search" place here="Enter a city ..." onChange={updateCity} />
-                <button type="Submit">Search</button>
-            </form>
-            );
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
 
-            if (result) {
-                return (
-                    <div>
-                        {form}
-                        <ul>
-                            <li> Temperature: {Math.round(weather.temperature)}°C</li>
-                            <li> Description: {weather.description}</li>
-                            <li>humidity{weather.humidity}%</li>
-                            <li> Wind: {weather.wind}km/h</li>
-                            <li>
-                                <img src={weather.icon} alt={weather.description} />
-                            </li>
-                        </ul>
-                    </div>
-                );
-            } else {
-                return form;
-            }
+  if (result) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,35 @@
-import React, { useLayoutEffect, useState } from "react";
-import axios from "axios";
-import Search from "./Search";
-import { Puff } from "react-loader-spinner";
+import React, { useState } from "react";
 
 export default function Weather(props) {
   const [description, setDescription] = useState("");
   const [humidity, setHumidity] = useState("");
-  const {wind, setWind} = useState("");
-  const [temperature, setTemperature] = useState("");
+  const [wind, setWind] = useState("");
 
-function showDescription(response) {
-  setDescription(response.data.main.description);
-}
   function showTemperature(response) {
-  setTemperature(response.data.main.temp);
+    setTemperature(response.data.main.temp);
+  }
+  function showDescription(response) {
+    setDescription(response.data.main.description);
+  }
+  function showHumidity(response) {
+    setHumidity(response.data.main.humidity);
+  }
+  function showWind(response) {
+    setWind(response.data.main.wind);
+  }
+  if (temperature) {
+    return (
+      <ul>
+        <li>Temperature: {Math.round(temperature)}°C </li>
+        <li>Description: {description} </li>
+        <li>Humidity: {humidity}% </li>
+        <li>Wind: {wind} km/h </li>
+      </ul>
+    );
+  } else {
+    axios.get(apiUrl).then(showTemperature);
+    axios.get(apiUrl).then(showDescription);
+    axios.get(apiUrl).then(showHumidity);
+    axios.get(apiUrl).then(showWind);
+  }
 }
-function showHumidity (reposnse) {
-  setHumidity(reposnse.data.main.humidity);
-}
-function showWind(response) {
-  setWind(response.data.main.wind);
-}
-
-if (temperature) {
-  return (
-    <ul>
-      <li> Temperature: {Math.round(temperature)}°C</li>
-      <li> Description: {description}</li>
-      <li>humidity{humidity}%</li>
-      <li> Wind: {wind}km/h</li>
-    </ul>
-  );
-} else {
-  let apiKey = "fe80b84fc85eea0c9b1fafa5aa3bbced";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showTemperature);
-  axios.get(apiUrl).then(showDescription);
-  axios.get(apiUrl).then(showHumidity);
-  axios.get(apiUrl).then(showWind);
-}
-}
-
- 
